f8df76bb53b5e473168fe37a553a6b3ddd2d1de3
Joshua Moody <joshuajmoody@gmail.com>
2013-10-03 00:30:34 +0200

    removed NSOperations to obtain gem version information because they were causing hanging at launch.
    * removed the app zip file because git was complaining it was too
      large (> 50M)
    * replaced app zip with a download link in the README.md
    * fixed a problem with the versioning strategy (build settings pointed
      to wrong location for InfoPlist.h)

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Calabash.app
 
+download the latest from here: 
+
+https://s3.amazonaws.com/littlejoysoftware/public/Calabash.app.zip
+
 ## Setup
 
 When first cloning this repo, be sure to run the following:
@@ -7,46 +11,45 @@ When first cloning this repo, be sure to run the following:
     1. gem install cocoapods
     2. pod install
     3. bundle install
-    4. bundle exec rake<sup>*</sup>
+    4. rake
     5. open Tokaido.xcworkspace
-
-`*` i am not sure if `bundle exec rake` or `rake` is appropriate.  there is/could be some weirdness around using `bundle` if you have local overrides declared in your `~/.bundle/config` file (like i do).  you have been warned.  see below for some other notes on using `bundle` in the Calabash shell.
 
 You should then be able to build and run Calabash in Xcode.
 
-### how it works
+## how it works
 
 there is a bunch of stuff under the hood that did not bother to understand.  i stripped out stuff that was not relevant to getting a shell up with ruby + calabash + dependent gems installed.
 
 the setup steps above install ruby and gems into a local directory that is used later to build a sandbox.
 
-at application launch, a sandbox directory is created using NSTemporaryDirectory.  the pre-installed ruby (2.0) and the calabash ios and android gems are expanded to that directory.  when the user touches the Start Terminal button in the UI, a shell is opened and the `PATH` variable is massaged so that the sandbox directory because the `GEM_HOME` and the pre-installed ruby becomes the default ruby.
+at application launch, a sandbox directory is created using `NSTemporaryDirectory`.  the pre-installed ruby (2.0) and the calabash ios and android gems are expanded to that directory.  when the user touches the Start Terminal button in the UI, a shell is opened and the `PATH` variable is massaged so that the sandbox directory becomes the `GEM_HOME` and the pre-installed ruby becomes the default ruby.
 
-### changing the ruby version
+## changing the ruby version
 
 i don't know how to do this.
 
-### changing the calabash version
+## changing the gem versions
 
 1. edit the `Tokaido/Gemfile` 
-2. `$ rm -rf ./tmp` or `rake clean`
+2. `rake clean`
 3. follow steps 3 and 4 in Setup (above)
-4. build and run the Calabash scheme in Xcode
+4. **CHANGE THE GEM VERSION INFO IN THE MainWindow.xib**
+5. build and run the Calabash scheme in Xcode
 
-**IMPORTANT** it is probably a great idea to repeat steps 2 - 3 above when you change branches.  i created a rake task `clean` that deletes the `./tmp` directory.
 
+**IMPORTANT**  repeat steps 2 - 3 above when you change branches. 
 
-### changing the About view
+## changing the About view
 
 i punted on this.  you can edit the `Tokaido/en.lproj/Credits.rtf`  to change the content.
 
 if you want something different, provide a layout/design.
 
-### changing the Terminal welcome message
+## changing the Terminal welcome message
 
 see `Tokaido/SetupTokaido.sh`
 
-### test
+## test
 
 1. archived a build
 2. signed and save the Calabash.app to /Applications/
@@ -61,7 +64,7 @@ see `Tokaido/SetupTokaido.sh`
   - iPhone 4S iOS 6
   - iOS 7 Simulator
 
-### bundler and global ~/.bundle/config
+## bundler and global ~/.bundle/config
 
 i use bundler to pin various projects to specific calabash-cucumber/calabash-android revisions.
 
@@ -75,9 +78,7 @@ the solution?  do not run with `bundle exec`
 
 in fact, if you use bundle to override gem installations, you should not use `bundle exec`, `bundle update`, or `bundle install` or super ruby madness will ensue.
 
-### the app
 
-i included a signed app in the repo `./Calabash.app.zip`
 
 
 
--- Tokaido/TKDTokaidoController.m
+++ Tokaido/TKDTokaidoController.m
@@ -1,42 +1,19 @@
-//
-//  TKDTokaidoController.m
-//  Tokaido
-//
-//  Created by Mucho Besos on 10/23/12.
-//  Copyright (c) 2012 Tilde. All rights reserved.
-//
-
 #import "TKDAppDelegate.h"
 #import "TKDTokaidoController.h"
 #import "TKDMuxrManager.h"
 #import "TKDApp.h"
-#import "LjsUnixOperation.h"
-#import "LjsUnixOperationResult.h"
 
+@interface TKDTokaidoController ()
 
-static NSString *const kCalabashIOSVersion = @"com.xamarin.Calabash - calabash iOS version";
-static NSString *const kCalabashAndroidVersion = @"com.xamarin.Calabash - calabash Android version";
-static NSString *const kCalabashRubyVersion = @"com.xamarin.Calabash - ruby version";
-static NSString *const kCalabashXamarinTestCloudVersion = @"com.xamarin.Calabash - xamarin test cloud version";
-
-@interface TKDTokaidoController () <LjsUnixOperationCallbackDelegate>
-
-@property (nonatomic, strong, readonly) NSOperationQueue *opqueue;
 @property (nonatomic, strong, readonly) NSView *mask;
 @property (nonatomic, strong, readonly) NSProgressIndicator *progressIndicator;
 
-
-- (void) launchVersionOperationWithPath:(NSString *) aLaunchPath
-                             identifier:(NSString *) aIdentifer
-                              arguments:(NSArray *) aArgs
-                            environment:(NSDictionary *) aEnvironment;
 @end
 
 @implementation TKDTokaidoController
 
 #pragma mark - Memory Management
 
-@synthesize opqueue = _opqueue;
 @synthesize mask = _mask;
 @synthesize progressIndicator = _progressIndicator;
 
@@ -48,141 +25,17 @@ static NSString *const kCalabashXamarinTestCloudVersion = @"com.xamarin.Calabash
     return self;
 }
 
-- (NSOperationQueue *) opqueue {
-    if (_opqueue != nil) { return _opqueue; }
-    _opqueue = [[NSOperationQueue alloc] init];
-    return _opqueue;
-}
 
 - (void) handleDidFinishInstallingSandbox {
     NSLog(@"DEBUG: sandbox did finish installing");
-
-    NSString *rubyDir = [TKDAppDelegate tokaidoInstalledRubiesDirectory];
-    NSString *rubySub = [NSString stringWithFormat:@"%@/bin/ruby", kTKDInstalledRubyVersion];
-    NSString *rubyPath = [rubyDir stringByAppendingPathComponent:rubySub];
-    NSArray *launchArgs = @[@"--version"];
-    [self launchVersionOperationWithPath:rubyPath
-                              identifier:kCalabashRubyVersion
-                               arguments:launchArgs
-                             environment:nil];
-
-
-    NSString *gemDir = [TKDAppDelegate tokaidoInstalledGemsDirectory];
-    NSString *gemBin = [gemDir stringByAppendingPathComponent:@"bin"];
-    
-    NSString *cal_ios = [gemBin stringByAppendingPathComponent:@"calabash-ios"];
-    NSString *cal_and = [gemBin stringByAppendingPathComponent:@"calabash-android"];
-    NSString *test_cloud = [gemBin stringByAppendingPathComponent:@"test-cloud"];
-  
-    
-    NSDictionary *env = @{@"GEM_HOME" : gemDir,
-                          @"GEM_PATH" : gemDir,
-                          @"RUBYPATH" : rubyPath,
-                          @"HOME" : NSHomeDirectory()};
-    
-    [self launchVersionOperationWithPath:rubyPath
-                              identifier:kCalabashIOSVersion
-                               arguments:@[cal_ios, @"version"]
-                             environment:env];
-    
-    [self launchVersionOperationWithPath:rubyPath
-                              identifier:kCalabashAndroidVersion
-                               arguments:@[cal_and, @"version"]
-                             environment:env];
-    
-    [self launchVersionOperationWithPath:rubyPath
-                              identifier:kCalabashXamarinTestCloudVersion
-                               arguments:@[test_cloud, @"version"]
-                             environment:env];
-
-    /*** UNEXPECTED ***
-    
-     we want to wait for the gems and ruby to report the versions.
-     
-     with some effort i could stop the animation and remove the mask _after_
-     all the 'version tasks' had completeed.
-     
-     better (from my POV) to let the user touch the button and have the app
-     blow up if it needs to.
-     
-     put another way - we ALWAYS want the mask and progress indicator to be 
-     removed, regardless of whether or not the version tasks complete
-     
-     NB: the bulk of launch time is spent unzipping the gems to the tmp directory
-     
-     1.0 is about the minimum for all the gems to report back
-     ******************/
-     __weak typeof(self) wself = self;
-    double delayInSeconds = 1.0;
-    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
-    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-        NSLog(@"DEBUG: removing progress indicator and mask");
-        NSProgressIndicator *pi = [wself progressIndicator];
-        [pi stopAnimation:nil];
-        NSView *mask = [wself mask];
-        [mask removeFromSuperview];
-        NSButton *stb = [wself buttonStartTerminal];
-        [stb setEnabled:YES];
-    });
+    NSLog(@"DEBUG: removing progress indicator and mask");
+    NSProgressIndicator *pi = [self progressIndicator];
+    [pi stopAnimation:nil];
+    NSView *mask = [self mask];
+    [mask removeFromSuperview];
+    NSButton *stb = [self buttonStartTerminal];
+    [stb setEnabled:YES];
 }
-
-
-- (void) launchVersionOperationWithPath:(NSString *) aLaunchPath
-                             identifier:(NSString *) aIdentifer
-                              arguments:(NSArray *) aArgs
-                            environment:(NSDictionary *) aEnvironment {
-    NSLog(@"launching version operation: '%@'", aIdentifer);
-    LjsUnixOperation *operation = [[LjsUnixOperation alloc]
-                                   initWithLaunchPath:aLaunchPath
-                                   launchArgs:aArgs
-                                   commonName:aIdentifer
-                                   callbackDelegate:self];
-    if (aEnvironment) { operation.task.environment = aEnvironment; }
-    
-    [self.opqueue addOperation:operation];
-}
-
-#pragma mark - Ljs Unix Operation Callback Delegate
-
-- (void) operationCompletedWithName:(NSString *)aName result:(LjsUnixOperationResult *)aResult {
-  
-    NSLog(@"DEBUG: received result for '%@'", [aResult commonName]);
-    NSLog(@"DEBUG: with exit code = '%ld'", [aResult exitCode]);
-    if ([aResult exitCode] != 0) {
-        NSLog(@"ERROR: %@", [aResult errOutput]);
-    }
-    
-    NSTextField *textField = nil;
-    if ([kCalabashIOSVersion isEqualToString:aName]) {
-        NSLog(@"DEBUG: received IOS version: '%@'", [aResult stdOutput]);
-        textField = [self labelIOSVersion];
-    } else if ([kCalabashAndroidVersion isEqualToString:aName]) {
-        NSLog(@"DEBUG: received Android version: '%@'", [aResult stdOutput]);
-        textField = [self labelAndroidVersion];
-    } else if ([kCalabashRubyVersion isEqualToString:aName]) {
-        NSLog(@"DEBUG: received Ruby version: '%@'", [aResult stdOutput]);
-        textField = [self labelRubyVersion];
-    } else if ([kCalabashXamarinTestCloudVersion isEqualToString:aName]) {
-        NSLog(@"DEBUG: received test cloud version: '%@'", [aResult stdOutput]);
-        textField = [self labelXamTestCloudVersion];
-    } else {
-        NSLog(@"ERROR: did not recognize operation with name: '%@'", aName);
-        NSLog(@"ERROR: dropping result on the floor");
-        return;
-    }
-    if (textField != nil) {
-        NSString *version = [aResult stdOutput];
-        if ([kCalabashRubyVersion isEqualToString:aName]) {
-            NSArray *tokens = [[aResult stdOutput] componentsSeparatedByString:@" "];
-            if ([tokens count] > 2) {
-                version = [tokens objectAtIndex:1];
-            }
-        }
-        
-        [textField setStringValue:version];
-    }
-}
-
 
 
 - (IBAction) openTerminalPressed:(id)sender {
@@ -190,12 +43,6 @@ static NSString *const kCalabashXamarinTestCloudVersion = @"com.xamarin.Calabash
     NSString *path = NSHomeDirectory();
     
     [delegate openTerminalWithPath:path];
-}
-
-
-- (void)didEndSheet:(NSWindow *)sheet returnCode:(NSInteger)returnCode contextInfo:(void *)contextInfo
-{
-    [sheet orderOut:self];
 }
 
 
